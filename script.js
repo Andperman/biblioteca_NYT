24cc0a09a2814dcf28e2e0173e07fc75db1e4433
Andrea Perez Manzaneque <andperman@Ordenador-portatil-de-Andrea.local>
2024-10-30 15:37:17 +0100

    conf_functions/firebase

--- script.js
+++ script.js
@@ -88,3 +88,133 @@ async function verLibros(listName) {
   });
   
 
+/* -----------------------------FIREBASE-------------------------*/
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js";
+import { getFirestore, collection, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-storage.js";
+
+// Configuración de Firebase
+let firebaseConfig = {
+  apiKey: "AIzaSyAUvIKU2cDhCNJjoBDuofvzliZOz0IpcjA",
+  authDomain: "demoweb-9bffc.firebaseapp.com",
+  projectId: "demoweb-9bffc",
+  storageBucket: "demoweb-9bffc.appspot.com",
+  messagingSenderId: "871348645073",
+  appId: "1:871348645073:web:f31a8a47cdd694c97f69f0"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore(app);
+const storage = getStorage();
+
+
+
+const signUpForm = document.getElementById('signup-form');
+const loginForm = document.getElementById('login-form');
+const logoutButton = document.getElementById('logout');
+const userData = document.getElementById('user-data');
+
+
+// Función para mostrar datos del usuario
+function displayUserData(user) {
+  const docRef = doc(db, "users", user.email);
+  getDoc(docRef).then(docSnap => {
+    if (docSnap.exists()) {
+      userData.style.cssText = 'background-color: #73AB84; width: 50%; margin: 2rem auto; padding: 1rem; border-radius: 5px; display: flex; flex-direction: column; align-items: center';
+      userData.innerHTML = `<h3>User Data</h3>
+                                  <p>Username: ${docSnap.data().username}</p>
+                                  <p>Email: ${docSnap.data().email}</p>
+                                  <img src="${docSnap.data().profile_picture}" alt="User profile picture">`;
+     
+    }
+  });
+}
+
+// Función para registrar un nuevo usuario
+if (document.body.contains(signUpForm)) {
+  signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const signUpEmail = document.getElementById('signup-email').value;
+    const signUpPassword = document.getElementById('signup-pass').value;
+    const signUpUser = document.getElementById('signup-user').value;
+    const signUpImg = document.getElementById('signup-picture').files[0];
+    const storageRef = ref(storage, signUpImg.name);   
+    //Se crea una referencia en Firebase Storage para la imagen del usuario, nombrada como el archivo original 
+    //(signUpImg.name). Esto permite almacenar 
+    //la imagen de perfil en una ruta específica en el almacenamiento.
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword);
+      console.log('User registered');
+      signUpForm.reset();
+
+      await uploadBytes(storageRef, signUpImg);
+      const publicImageUrl = await getDownloadURL(storageRef);
+
+      await setDoc(doc(collection(db, "users"), signUpEmail), {
+        username: signUpUser,
+        email: signUpEmail,
+        profile_picture: publicImageUrl
+      });
+      console.log("Document written in Firestore");
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  });
+}
+
+
+// Función para iniciar sesión
+if (document.body.contains(loginForm)) {
+  loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const loginEmail = document.getElementById('login-email').value;
+    const loginPassword = document.getElementById('login-pass').value;
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+      console.log('User authenticated');
+      loginForm.reset();
+      displayUserData(userCredential.user);
+    } catch (error) {
+      document.getElementById('msgerr').innerHTML = 'Invalid user or password';
+      console.log('Error: ', error);
+    }
+  });
+}
+
+// Función para cerrar sesión 
+if (document.body.contains(logoutButton)) {
+  logoutButton.addEventListener('click', () => {
+    const confirmation = confirm("¿Desea salir del perfil?");
+    if (confirmation) {
+      signOut(auth).then(() => {
+        console.log('User logged out');
+        userData.style.cssText = '';
+        userData.innerHTML = '';
+      }).catch((error) => {
+        console.log('Error: ', error);
+      });
+    }
+  });
+}
+
+// Función para observar el estado de autenticación
+function observeAuthState() {
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      console.log('Logged user');
+      displayUserData(user);
+    } else {
+      console.log('No logged user');
+      userData.style.cssText = '';
+      userData.innerHTML = '';
+    }
+  });
+}
+
+observeAuthState();
